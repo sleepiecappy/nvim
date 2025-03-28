c10b63d8d7f327afe0a1d60edf837c1684faceaf
Evangelista <evangelistajoaop@gmail.com>
2025-03-28 15:25:24 -0300

    Change theme variant to mocha

--- lua/lazy/plugins/colorscheme.lua
+++ lua/lazy/plugins/colorscheme.lua
@@ -37,7 +37,7 @@ return { -- You can easily change to a different colorscheme.
         },
       },
     }
-    local cs = 'catppuccin-latte'
+    local cs = 'catppuccin-mocha'
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
