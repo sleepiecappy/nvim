df5f76fbae6acf1e0385163538c90ee31ca225b6
Evangelista <evangelistajoaop@gmail.com>
2025-06-19 18:53:43 -0300

    comment out notify, it was giving warnings?

--- lua/config/cmd.lua
+++ lua/config/cmd.lua
@@ -1,14 +1,14 @@
 vim.api.nvim_create_user_command('SetTitle', function()
   local cwd = vim.fn.getcwd()
   if not cwd then
-    vim.notify('Could not get current working directory.', vim.log.levels.ERROR)
+    -- vim.notify('Could not get current working directory.', vim.log.levels.ERROR)
     return
   end
 
   local dir_name = vim.fs.basename(cwd)
 
   if dir_name == nil or dir_name == '' then
-    vim.notify('Could not determine directory name from: ' .. cwd, vim.log.levels.ERROR)
+    -- vim.notify('Could not determine directory name from: ' .. cwd, vim.log.levels.ERROR)
     return
   end
 
@@ -16,7 +16,7 @@ vim.api.nvim_create_user_command('SetTitle', function()
 
   vim.system(cmd_args, { text = true }, function(obj)
     if obj.code == 0 then
-      vim.notify('WezTerm tab title set to: ' .. dir_name, vim.log.levels.INFO)
+      -- vim.notify('WezTerm tab title set to: ' .. dir_name, vim.log.levels.INFO)
     else
       local errmsg = 'Failed to set WezTerm tab title.'
       if obj.stderr and obj.stderr ~= '' then
@@ -26,7 +26,7 @@ vim.api.nvim_create_user_command('SetTitle', function()
       else
         errmsg = errmsg .. ' Exit code: ' .. obj.code
       end
-      vim.notify(errmsg, vim.log.levels.ERROR)
+      -- vim.notify(errmsg, vim.log.levels.ERROR)
     end
   end)
 end, { desc = 'Set wezterm tab title' })
