41026cf4d2f6c01d449262989e656095e4029804
Evangelista <evangelistajoaop@gmail.com>
2025-07-07 15:51:20 -0300

    renable dap

--- lua/lazy/plugins/debug.lua
+++ lua/lazy/plugins/debug.lua
@@ -7,18 +7,12 @@
 -- kickstart.nvim and not kitchen-sink.nvim ;)
 
 return {
-  -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
-  enabled = false,
-  -- NOTE: And you can specify dependencies as well
+  enabled = true,
   dependencies = {
-    -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
     'theHamsta/nvim-dap-virtual-text',
-    -- Required dependency for nvim-dap-ui
     'nvim-neotest/nvim-nio',
-
-    -- Installs the debug adapters for you
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
 
